925115502f8cd5079854a8e31241509284703979
Grace Kim <goooyoon@gmail.com>
2020-06-01 17:36:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,12 +1,9 @@
 
 def hopper
 	programmer_hash = 
- 		{ :grace_hopper => {:known_for => "COBOL",
+ 		{:known_for => "COBOL",
      :languages => ["COBOL", "FORTRAN"]
         }
-     }
-
-
 end
 
 def alan_kay_is_known_for
