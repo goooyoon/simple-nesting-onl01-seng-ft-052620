d9a56894dbf4c425ebcdd5afc3e4a5808301e262
Grace Kim <goooyoon@gmail.com>
2020-06-01 17:42:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -42,7 +42,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
      
